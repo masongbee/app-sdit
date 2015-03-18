bd97f1347b7777e5ff9d7e0a8f2a9e6893f01168
masongbee <ma.songbee@gmail.com>
2015-03-19 06:03:17 +0700

    test2
    test2

--- application/controllers/welcome.php
+++ application/controllers/welcome.php
@@ -28,7 +28,7 @@ class Welcome extends CI_Controller {
 	 */
 	public function index()
 	{
-		$this->load->view('welcome_message');
+		$this->load->view('welcome_message');//hanya test
 	}
 	
 	function test(){
